55fcab25d3a6b4eba5ece05a34bc2f5a6b50354d
haozi <740836086@qq.com>
2016-11-18 06:40:18 +0000

    add descriptor file

---
+++ select/selectComprehended.c
@@ -0,0 +1,13 @@
+/**
+	the function of select and invoke sequence of select
+	
+	使用select 函数时可以将多个文件描述符集中到一起监视  如下:
+	1. 是否存在套接字接收数据
+	2. 无需阻塞传输数据的套接字有哪些
+	3. 哪些套接字发生了异常 ?
+	
+	but  how  to understand the event ?
+	监视项称为 "事件" 发生监视项对应情况时 称"发生了事件"
+	这是最常见的表达。
+
+*/
